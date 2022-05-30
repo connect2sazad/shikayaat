34439e10320d5c237b23c9bb74b70b5249f9b4cf
Sazad Ahemad <mail2sazad@gmail.com>
2022-05-30 19:37:44 +0530

    added file a complaint functionality

--- file-a-complaint/index.php
+++ file-a-complaint/index.php
@@ -16,14 +16,59 @@ include_once "../assets/asset_manager.php";
 <body>
     <main>
         <?php
-            get_header('../');
+        get_header('../');
         ?>
         <div class="section-holder">
             <?php
-                get_sidebar('file-a-complaint');
+            get_sidebar('file-a-complaint');
             ?>
             <section class="right-content-section" id="right-content-section">
-                
+
+                <div class="content-wrapper">
+
+                    <div>
+                        <h1>File a Complaint</h1>
+                        <!-- <form> -->
+                        <div class="input-control">
+                            <label for="userid">Title</label>
+                            <input type="text" id="userid" name="userid" autocomplete="off" spellcheck="false" required aria-required="true">
+                        </div>
+                        <div class="input-control">
+                            <label for="responsible-authority">Responsible Authority</label>
+                            <select id="responsible-authority">
+                                <option value="none" selected disabled>Select</option>
+                                <option value="Examination Department">Examination Department</option>
+                                <option value="Transportation Department">Transportation Department</option>
+                            </select>
+                        </div>
+                        <div class="input-control">
+                            <label for="priority">Priority</label>
+                            <select id="priority">
+                                <option value="none" selected disabled>Select</option>
+                                <option value="high">High</option>
+                                <option value="medium">Medium</option>
+                                <option value="low">Low</option>
+                            </select>
+                        </div>
+                        <div class="input-control">
+                            <label for="description">Problem Description</label>
+                            <textarea></textarea>
+                        </div>
+                        <div class="input-control">
+                            <label for="suggestions">Suggestions</label>
+                            <textarea></textarea>
+                        </div>
+                        <div class="input-control">
+                            <label for="file-input">Attachments</label>
+                            <input type="file" id="file-input" multiple>
+                        </div>
+                        <div class="input-control">
+                            <input type="submit" class="submit-btn" value="COMPLAIN" />
+                        </div>
+                    </div>
+
+                </div>
+
             </section>
         </div>
     </main>
